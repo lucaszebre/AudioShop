b6a0c73013f0771adad46cffd293db7b29c34c34
lucaszebre <yakirisk@gmail.com>
2023-10-12 19:07:03 +0200

    add strip route

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,35 @@ import router from './routerPrivate'
 import morgan from 'morgan'
 import cors from 'cors'
 import { protect } from './modules/auth'
-import { createNewUser, signin } from './handlers/user'
-import { getProducts,getOneProduct } from './handlers/product'
 import routerer from './routerPublic'
 const app = express()
+require("dotenv").config();
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-
 app.use(routerer)
 app.use( protect, router)
-// app.post('/register', createNewUser)
-// app.post('/login', signin)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// Fetch the publishable key to initialize Stripe.js
+app.get("/publishable-key", () => {
+  return { publishable_key: process.env.STRIPE_PUBLISHABLE_KEY };
+})
+
+// Create a payment intent and return its client secret
+app.post("/create-payment-intent", async () => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "eur",
+    payment_method_types: ["bancontact", "card"],
+  });
+
+  return { client_secret: paymentIntent.client_secret };
+});
+
 app.use((err, req, res, next) => {
   console.log(err)
   res.json({message: `had an error: ${err.message}`})

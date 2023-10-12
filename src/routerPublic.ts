d3ce81fca6c464074d817bf44844af9c8d7dfb28
lucaszebre <yakirisk@gmail.com>
2023-10-12 10:11:13 +0200

    dd

--- src/routerPublic.ts
+++ src/routerPublic.ts
@@ -4,6 +4,7 @@ import { createNewUser, signin } from './handlers/user';
 import { getProducts,getOneProduct } from './handlers/product';
 
 const router = Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/register', createNewUser);
 router.post('/login', signin);
